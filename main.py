58edb432b449638168bccfd94ca1fddf77423bae
Serg <tumanovser@mail.ru>
2020-12-28 09:28:35 +0300

    16

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+import sys
+
+WIDTH = 8
+HEIGHT = 8
+
+
+def drawBoard(board):
+    print('  12345678')
+    print(' +--------+')
+    for y in range(HEIGHT):
+        print('%s|' % (y+1), end='')
+        for x in range(WIDTH):
+            print(board[x][y], end='')
+        print('|%s' % (y+1))
+    print(' +--------+'
+          print('  12345678'))
